76e82a4f22f372c205718e92bc20529667edb5b3
Bruno Mazzocchi <brunomazzocchidev@gmail.com>
2024-05-12 12:49:18 -0600

    Add not found middleware

--- src/app.js
+++ src/app.js
@@ -55,6 +55,11 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something went wrong!");
 });
 
+// Not found middleware
+app.use((req, res, next) => {
+  res.status(404).send("Not found");
+});
+
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
